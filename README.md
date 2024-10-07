19112c299e1ee1651b4ff4e7fc9ce7f1e58ab58e
Troy Washington <troyvw96@gmail.com>
2024-10-07 13:51:00 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Add the following to this `README.md` file:  
   * A short report explaining your design decisions and any challenges faced.
   * Documentation: Clear and concise documentation on how to run your application.
+
+I used const for every variable that was able to remain as a constant while still allowing for the following code to run without error. Const is always the best way to declare variables as this will allow for more clarity in objects reference as well as a more predictable and easier to reason with code. Arrays are always genereally declared as const because they can still be edited without error. I declared all array objects as const and I used let for variables that were either made to be reassigned or would be explicitly used within one a specific block of code and no other. 
--- declaration.js
+++ declaration.js
@@ -1,23 +1,30 @@
-var fullName = "John Doe"; 
+// This variable is changed immediately after declaration so I use var. 
+let fullName = "John Doe";
+// If const this variable would not be allowed to change value from "John Doe" to "Jane Doe".
 fullName = "Jane Doe";
 console.log(fullName);
 
-
-var age = 30; 
+// I used const here because the variable does not change, also the variable is used as part of the codes argument.  
+const age = 30;
 if (age > 18) {
-  var adult = true; 
-  console.log(adult); 
-}  
+  // let because the variable will only be used within this block of code. 
+  let adult = true;
+  console.log(adult);
+}
 
-var loopArray = []; 
-for (var i = 0; i < 5; i++) {
+// const prevents reassigning of the variable while still allowing for you to add, remove, or change of array elements. 
+const loopArray = [];
+// let because the value of the variable is being reassigned after each iteration of loop. 
+for (let i = 0; i < 5; i++) {
   loopArray.push(i);
-}  
+}
 console.log(loopArray);
 
-var MAXIMUM = 100;
+// I would use let because the value of the variable is being reaassigned.
+let MAXIMUM = 100;
 MAXIMUM = 200;
 
-var colors = ["Red", "Green", "Blue"];
+// this is an array so we want the name 'colors' to refer to the array colors. Values will still be added, removed, and edited with no issue with this array as a constant. Generally arrays are always declared as a constant. 
+const colors = ["Red", "Green", "Blue"];
 colors = ["Yellow", "Pink", "Purple"];
 console.log(colors);
